254cdc2bfd258bb7b54cb759258eb53cf19808a4
Aadesh Parija <aadesh@example.com>
2026-02-04 01:02:18 +0530

    adding ---

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // Production environment configuration
 export const environment = {
   production: true,
-  apiUrl: 'https://api.hrms-lite.com',
+  apiUrl: 'https://hrms-lite-backend-testing.onrender.com',
   apiEndpoints: {
     employees: '/api/employees/',
     attendance: '/api/attendance/'
